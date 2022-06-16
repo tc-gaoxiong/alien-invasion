3c40016bca06b2cf3f5bd21818c2b344b0053f6f
gaoxiong <2532984942@qq.com>
2022-06-16 16:35:51 +0800

    重构子弹逻辑

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,14 +38,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # 删除消失的子弹
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            # print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
     
     # 这是约定，以下划线开头表示该方法或变量仅供内部使用
@@ -84,6 +77,17 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹并删除消失的子弹"""
+        # 更新子弹的位置
+        self.bullets.update()
+
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
