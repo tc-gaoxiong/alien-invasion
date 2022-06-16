1ccb00708860907026f86e1ed43f40c8b451713f
gaoxiong <2532984942@qq.com>
2022-06-16 16:48:34 +0800

    创建 Alien

--- alien.py
+++ alien.py
@@ -10,7 +10,7 @@ class Alien(Sprite):
         self.screen = ai_game.screen
 
         # 加载外星人图像并设置其 rect 属性
-        self.image = pygame.image.load('image/aline.bmp')
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
         # 每个外星人最初都在屏幕左上角附近
--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -29,6 +30,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         # 设置背景色
         # self.bg_color = (230, 230, 230) # RGB
@@ -88,13 +92,21 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
+    
+    def _create_fleet(self):
+        """创建外星人群"""
+        alien = Alien(self)
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        
+        self.aliens.draw(self.screen)
         
         # 让最近绘制的屏幕可见
         pygame.display.flip()
