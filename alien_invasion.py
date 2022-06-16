321ee9ad6927f7ff677b11756426f180201f5441
gaoxiong <tcgaoxiong@163.com>
2022-06-16 09:52:15 +0800

    新增设置模块

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
@@ -8,14 +10,19 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         # 初始化背景设置，让 pygame 能够正确的工作
         pygame.init()
+        self.settings = Settings()
 
         # 创建一个显示窗口，游戏的所有图形元素都将在其中绘制
         # display.set_mode 返回 surface 对象，表示整个窗口
-        self.screen = pygame.display.set_mode((1200, 800)) # 指定窗口大小
+        # self.screen = pygame.display.set_mode((1200, 800)) # 指定窗口大小
+        self.screen = pygame.display.set_mode(
+            self.settings.screen_width, self.settings.screen_height
+        )
+
         pygame.display.set_caption("Alien Invasion")
 
         # 设置背景色
-        self.bg_color = (230, 230, 230) # RGB
+        # self.bg_color = (230, 230, 230) # RGB
     
     def run_game(self):
         """开始游戏的主循环"""
@@ -26,7 +33,7 @@ class AlienInvasion:
                     sys.exit()
 
             # 每次循环都重绘屏幕
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
         
             # 让最近绘制的屏幕可见
             # pygame 每次循环都将绘制一个空屏幕，并擦去就屏幕，使得只有新屏幕可见
