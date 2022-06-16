54ab1fe38a6b81a2cfcb802c8f531c48783e805b
gaoxiong <2532984942@qq.com>
2022-06-16 15:12:30 +0800

    调整移动速率

--- ship.py
+++ ship.py
@@ -9,6 +9,7 @@ class Ship:
         """初始化飞船并设置其初始位置"""
         
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取其外接矩形
@@ -22,12 +23,18 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
+        # 在飞船的属性 x 中存储小数值
+        self.x = float(self.rect.x)
+
     def update(self):
         """"根据移动标志调整飞船的位置"""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        # 根据 self.x 更新 rect 对象
+        self.rect.x = self.x
 
     def blitme(self):
         """在指定位置绘制飞船"""
